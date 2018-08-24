187f357cc7913268d9c147f68299413a3bb75284
Dante Solorio <ddasogaa@gmail.com>
2018-08-24 11:20:07 -0500

    Example of modification

--- EasyAnchors/MasterViewController.swift
+++ EasyAnchors/MasterViewController.swift
@@ -23,7 +23,7 @@ class MasterViewController: UIViewController {
 
 extension MasterViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 15
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
